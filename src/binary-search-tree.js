a959e49da160f95d067a171b68a220cca2926086
Rahimjon <abduraximovrahimjon8@gmail.com>
2025-03-02 17:51:24 +0500

    feat: finish all tasks

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,49 +1,101 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-// const { Node } = require('../extensions/list-tree.js');
+const { NotImplementedError } = require('../extensions/index.js')
+const { Node } = require('../extensions/list-tree.js')
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 class BinarySearchTree {
+  constructor() {
+    this.rootNode = null
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    const newNode = new Node(data)
+
+    if (!this.rootNode) {
+      this.rootNode = newNode
+      return
+    }
+
+    let current = this.rootNode
+    while (true) {
+      if (data < current.data) {
+        if (!current.left) {
+          current.left = newNode
+          return
+        }
+        current = current.left
+      } else if (data > current.data) {
+        if (!current.right) {
+          current.right = newNode
+          return
+        }
+        current = current.right
+      } else {
+        return
+      }
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return !!this.find(data)
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(data) {
+    let current = this.rootNode
+    while (current) {
+      if (data === current.data) return current
+      current = data < current.data ? current.left : current.right
+    }
+    return null
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    this.rootNode = this._removeNode(this.rootNode, data)
+  }
+
+  _removeNode(node, data) {
+    if (!node) return null
+
+    if (data < node.data) {
+      node.left = this._removeNode(node.left, data)
+    } else if (data > node.data) {
+      node.right = this._removeNode(node.right, data)
+    } else {
+      if (!node.left) return node.right
+      if (!node.right) return node.left
+
+      let minRight = this._findMin(node.right)
+      node.data = minRight.data
+      node.right = this._removeNode(node.right, minRight.data)
+    }
+    return node
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._findMin(this.rootNode)?.data || null
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._findMax(this.rootNode)?.data || null
+  }
+
+  _findMin(node) {
+    while (node?.left) node = node.left
+    return node
+  }
+
+  _findMax(node) {
+    while (node?.right) node = node.right
+    return node
   }
 }
 
 module.exports = {
-  BinarySearchTree
-};
+  BinarySearchTree,
+}
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require('../extensions/index.js')
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -15,18 +15,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be [1, 2, 4, 5]
  *
  * Singly - linked lists are already defined using interface
- * class ListNode {
- *   constructor(x) {
- *     this.value = x;
- *     this.next = null;
- *   }
- * }
+
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+class ListNode {
+  constructor(x) {
+    this.value = x
+    this.next = null
+  }
+}
+
+function removeKFromList(l, k) {
+  while (l !== null && l.value === k) {
+    l = l.next
+  }
+
+  let current = l
+
+  while (current !== null && current.next !== null) {
+    if (current.next.value === k) {
+      current.next = current.next.next
+    } else {
+      current = current.next
+    }
+  }
+
+  return l
 }
 
 module.exports = {
-  removeKFromList
-};
+  removeKFromList,
+}
